994373869a0cbd12391789e29104bb9217e1a720
Jiří Fryč <frycjiri@gmail.com>
2021-01-12 21:36:24 +0100

    Fixing jest tests.

--- test/webdriver.test.ts
+++ test/webdriver.test.ts
@@ -8,13 +8,13 @@ options.path = path.resolve(__dirname, 'drivers');
 
 test('Firefox', async () => {
   await WebdriverDownloader.firefox(options).download();
-  expect(fs.existsSync(path.resolve(options.path, 'geckodriver.exe'))).toBe(true);
+  expect(fs.existsSync(path.resolve(options.path as string, 'geckodriver.exe'))).toBe(true); //TODO: This will work only on win environment.
 });
 test('Chrome', async () => {
   await WebdriverDownloader.chrome(options).download();
-  expect(fs.existsSync(path.resolve(options.path, 'chromedriver.exe'))).toBe(true);
+  expect(fs.existsSync(path.resolve(options.path as string, 'chromedriver.exe'))).toBe(true); //TODO: This will work only on win environment.
 });
 test('Opera', async () => {
   await WebdriverDownloader.opera(options).download();
-  expect(fs.existsSync(path.resolve(options.path, 'operadriver.exe'))).toBe(true);
+  expect(fs.existsSync(path.resolve(options.path as string, 'operadriver.exe'))).toBe(true); //TODO: This will work only on win environment.
 });
